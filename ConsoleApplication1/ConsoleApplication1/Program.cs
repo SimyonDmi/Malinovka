3fbf66545dd076f51bef51787dc03e426de91a0e
Simyon <xartman90@mail.ru>
2018-02-15 09:25:55 +0700

    Privet

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -12,6 +12,7 @@ namespace ConsoleApplication1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Oh, Hi, Simyon");
+            Console.WriteLine("Oh, Hi, Roma Roma Roman");
             Console.ReadKey();
         }
     }
